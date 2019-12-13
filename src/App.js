e6daa2ce3308c6bebd62f4899d11bdc75da091ba
James Coyne <jamescoyne@Jades-MBP.localdomain>
2019-12-13 16:03:56 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import MainPanel from './main-panel/main-panel';
+import HistoryPanel from './history-panel/history-panel';
+import DiscountPanel from './discount-panel/discount-panel';
+import CashFlowsPanel from './cash-flows-panel/cash-flows-panel';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={{width: '100vw', height: '43vw', border: '1px solid black'}}>
+       <Grid container className={classes.root} spacing={2}>
+        <Grid item xs={12}>
+          <h1>NET PRESENT VALUE CALCULATOR</h1>
+        </Grid>
+        <Grid item>
+          <Grid container className={classes.root} spacing={3}>
+            <Grid item>
+              
+            </Grid>
+            <Grid item>
+              
+            </Grid>
+            <Grid item>
+              
+            </Grid>
+          </Grid>
+        </Grid>
+       </Grid>
+      
+      <DiscountPanel/>
+      <CashFlowsPanel/>
+
+      <MainPanel/>
+      <HistoryPanel/>
     </div>
   );
 }
---
+++ src/cash-flows-panel/cash-flows-panel.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+class CashFlowsPanel extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {};
+    }
+
+    componentDidMount() {
+
+    }
+
+    render() {
+        return (<div></div>)
+    }
+
+}
+
+export default CashFlowsPanel;
